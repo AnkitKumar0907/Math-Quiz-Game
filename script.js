af9276b3f30d70655d3880280708aa7d63698b1e
Ankit Kumar <Ankitkumarnokia1998@gmail.com>
2026-03-12 15:44:16 +0530

    Fix incorrect toast message for correct answer

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ const checkAnswer = (event) => {
   if (userAnswer === storedAnswer) {
     score += 1;
     Toastify({
-      text: `Your are wrong and your score is ${score}`,
+      text: `Your are correct and your score is ${score}`,
       gravity: "bottom",
       position: "center",
       style: {
